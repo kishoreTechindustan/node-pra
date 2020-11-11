d36ee3b8901f3131e77ca693b775be0a5ec40d58
kishoreTechindustan <kishore@techindustan.com>
2020-11-11 17:14:31 +0530

    enchancing app

--- routes/admin.js
+++ routes/admin.js
@@ -2,15 +2,21 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const rootDir = require('../util/path')
-const productsController = require('../controllers/product');
+const adminController = require('../controllers/admin');
 
 
 // /adimn/add-product => GET
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// /adimn/products => GET
+
+router.get('/products', adminController.getProducts);
 
 // /adimn/add-product => POST
-router.post('/add-product', productsController.postAddProduct)
+router.post('/add-product', adminController.postAddProduct)
+
+
 
 module.exports = router;
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const productsController = require('../controllers/product')
+const shopController = require('../controllers/shop')
 //== shop =>GET
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders)
+
+router.get('/checkout', shopController.getCheckout);
 
 
 module.exports = router;
